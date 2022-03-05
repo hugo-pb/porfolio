13cc06956a870107f143877502e47f96bd640767
Hugo <h.palomerabecerra@gmail.com>
2022-03-04 18:17:33 -0700

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Tabs, Tab } from "react-bootstrap";
-import "../Styles/About.css"
+import "../Styles/About.css";
 export default function About() {
   const [key, setKey] = useState("home");
   return (
@@ -10,11 +10,13 @@ export default function About() {
         <Tab eventKey="home" title="Why coding?">
           <p>
             "Challenge accepted!" since i was i little kid i love solving
-            puzzels, thinking out the box, and comming with unnique ideas. My brother was aways tech oriented but 
+            puzzels, thinking out the box, and comming with unnique ideas, but i
+            always said computers are not my thing.
           </p>
+          <p>Later on while working</p>
         </Tab>
         <Tab eventKey="coding" title="When Im not coding? ">
-          <p>hi</p>
+          <p>h</p>
         </Tab>
         <Tab eventKey="contact" title="Contact"></Tab>
       </Tabs>
