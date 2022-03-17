259a71ef9279469f6686438cb3cacae0281e6d27
Hugo <h.palomerabecerra@gmail.com>
2022-03-17 17:15:44 -0600

    netlify form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,21 +2,30 @@ import React from 'react'
 
 export default function Contact() {
   return (
-      <div className='container' id='Contact'>
-          <form>
-  <div className="mb-3">
-    <label for="exampleInputEmail1" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div className="mb-3">
-    <label for="exampleInputPassword1" className="form-label">Message</label>
-    <input type="message" className="form-control" id="exampleInputPassword1"/>
-  </div>
- 
-  <button type="submit" className="btn btn-primary">Submit</button>
-          </form>
-      
+    <div className="container" id="Contact">
+      <form  data-netlify="true">
+        <div className="mb-3">
+          <label className="form-label">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label for="Message" className="form-label">
+            Message
+          </label>
+          <input type="message" className="form-control" />
+        </div>
+        <div data-netlify-recaptcha="true"></div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </div>
-  )
+  );
 }
